52b55530ca213349ac071f970ce374c8cd56e970
Athensoft <inf.athensoft@hotmail.com>
2020-01-03 22:23:21 -0500

    added quiz

---
+++ quiz/quiz_05.py
@@ -0,0 +1,44 @@
+"""
+quiz 5
+"""
+
+# question 5
+num = 9
+print(isinstance(num, int))
+
+
+# question 6
+# 6-2
+my_list = [1,2,3,4,5,6]
+print(my_list[0],my_list[-1])
+print(my_list[0],my_list[5])
+
+# 6-3
+my_list[3] = 999
+print(my_list[3])
+
+# 6-4
+my_tuple = (1,2,3,4,5,6)
+print(my_tuple[0],my_tuple[-1])
+print(my_tuple[0],my_tuple[5])
+
+# 6-6
+my_set = {1,1,2,2,3,3}
+print(my_set)
+
+# 6-7
+my_dict = {'a': 1, 'b': 2}
+print(my_dict)
+
+
+# question 7
+f1 = 9.9
+print(str(f1))
+
+s1 = '99'
+print(int(s1))
+
+
+
+
+
