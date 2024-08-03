c665858ae2a168066cec50fae38ae565ddc56c0f
athensedu <lada314@gmail.com>
2024-08-02 20:59:07 -0400

    updated random project treasure chest

--- evaluate/evaluate_3_project/python2_intermediate/treasure_chest/mychest.py
+++ evaluate/evaluate_3_project/python2_intermediate/treasure_chest/mychest.py
@@ -9,37 +9,35 @@ org:        Athensoft Inc.
 import random
 
 
+
 # prepare a chest with 1000 possible items
 def create_chest():
-    chest = []
-    # for i in range(638):
-    #     chest.append(ITEM1)
-    # for i in range(250):
-    #     chest.append(ITEM2)
-    # for i in range(100):
-    #     chest.append(ITEM3)
-    # for i in range(10):
-    #     chest.append(ITEM4)
-    # for i in range(2):
-    #     chest.append(ITEM5)
-
     list1 = [ITEM1] * 638
     list2 = [ITEM2] * 250
     list3 = [ITEM3] * 100
     list4 = [ITEM4] * 10
     list5 = [ITEM5] * 2
-    chest = list1 + list2+list3+ list4+list5
-    random.shuffle(chest)
-    return chest
+
+    chests = list1 + list2 + list3 + list4 + list5
+
+    random.shuffle(chests)
+    return chests
 
 
 # trials
-def open_chests(t):
-    def get_item(mychest):
-        return random.choice(mychest)
+def open_chests(t, from_chests):
+    def get_item(chests):
+        return random.choice(chests)
+
+    rank1_items = []
+    rank2_items = []
+    rank3_items = []
+    rank4_items = []
+    rank5_items = []
 
     for i in range(t):
-        item = get_item(mychest)
+        item = get_item(from_chests)
+
         if item==ITEM1:
             rank1_items.append(item)
         elif item==ITEM2:
@@ -51,43 +49,62 @@ def open_chests(t):
         elif item==ITEM5:
             rank5_items.append(item)
 
+    return {
+        "rank1_items": rank1_items,
+        "rank2_items": rank2_items,
+        "rank3_items": rank3_items,
+        "rank4_items": rank4_items,
+        "rank5_items": rank5_items,
+    }
 
-# main program
 
+
+def main(times=500000):
+    # 1. create all chests
+
+    all_chests = create_chest()
+
+    # 2. open chests
+    # set how many times of trial
+    N = times
+    items_by_rank_dict = open_chests(N, all_chests)
+
+    # 3. get real probabilities
+    # pre-setting probabilities for items of each item
+    P1 = 0.638
+    P2 = 0.25
+    P3 = 0.1
+    P4 = 0.01
+    P5 = 0.002
+
+    p1 = len(items_by_rank_dict['rank1_items'])/N
+    p2 = len(items_by_rank_dict['rank2_items'])/N
+    p3 = len(items_by_rank_dict['rank3_items'])/N
+    p4 = len(items_by_rank_dict['rank4_items'])/N
+    p5 = len(items_by_rank_dict['rank5_items'])/N
+
+
+    # 4. Output to compare real probabilities with pre-setting probabilities
+    print(f"{N:,} times of trial")
+    category_width = 9  # max width
+
+    print(f'Probability of {"normal":<{category_width}} item is {p1:.4f}, and the pre-setting is {P1:.4f}, Diff is {p1-P1:>7.4f}')
+    print(f'Probability of {"magic":<{category_width}} item is {p2:.4f}, and the pre-setting is {P2:.4f}, Diff is {p2-P2:>7.4f} ')
+    print(f'Probability of {"rare":<{category_width}} item is {p3:.4f}, and the pre-setting is {P3:.4f}, Diff is {p3-P3:>7.4f} ')
+    print(f'Probability of {"legendary":<{category_width}} item is {p4:.4f}, and the pre-setting is {P4:.4f}, Diff is {p4-P4:>7.4f} ')
+    print(f'Probability of {"ancient":<{category_width}} item is {p5:.4f}, and the pre-setting is {P5:.4f}, Diff is {p5-P5:>7.4f} ')
+    print()
+
+# settings
 ITEM1 = 'N'
 ITEM2 = 'M'
 ITEM3 = 'R'
 ITEM4 = 'L'
 ITEM5 = 'A'
 
-rank1_items = []
-rank2_items = []
-rank3_items = []
-rank4_items = []
-rank5_items = []
-
-# set how many times of trial
-N = 500000
-
-mychest = create_chest()
-open_chests(N)
-
-# statistics
-P1 = 0.638
-P2 = 0.25
-P3 = 0.1
-P4 = 0.01
-P5 = 0.002
-
-p1 = len(rank1_items)/N
-p2 = len(rank2_items)/N
-p3 = len(rank3_items)/N
-p4 = len(rank4_items)/N
-p5 = len(rank5_items)/N
-
-# comparing
-print(f'Probability of normal item is {p1}, and the pre-setting is {P1}')
-print(f'Probability of magic item is {p2}, and the pre-setting is {P2}')
-print(f'Probability of rare item is {p3}, and the pre-setting is {P3}')
-print(f'Probability of legendary item is {p4}, and the pre-setting is {P4}')
-print(f'Probability of ancient legendary item is {p5}, and the pre-setting is {P5}')
+# main program
+main(10000)
+main(100000)
+main(500000)
+main(1000000)
+main(5000000)
